16bc0682e7449d3665cf32f70a3580fbffeb83ed
xup3 <1422640+xup3@users.noreply.github.com>
2022-03-08 18:27:41 +0100

    chore: initial commit

---
+++ src/__tests__/collision.spec.ts
@@ -0,0 +1,20 @@
+describe("Collision Test Suite", () => {
+  type Coords = {
+    x: number,
+    y: number
+  }
+
+  test("Will detect a collision of two objects", () => {
+    const coords1: Coords = {x: 1, y: 200}
+    const coords2: Coords = {x: 1, y: 200}
+
+    expect(coords1).toEqual(coords2)
+  })
+
+  test("The coordinations will not collide", () => {
+    const coords1: Coords = {x: 1, y: 300}
+    const coords2: Coords = {x: 1, y: 200}
+
+    expect(coords1).not.toEqual(coords2)
+  })
+})
